036b0708cdc6e6cc12e3a26a4c0e5ca0f3ea4097
Emma Cheng <ecolipp@gmail.com>
2020-04-24 21:20:01 +0800

    about and contact add home icon search v.1-8

--- js/main.js
+++ js/main.js
@@ -1,45 +1,47 @@
 $(document).ready(function(){
 	//nav bag
 	$('.li-1').click(function(e){
-		$(this).find('.dropdown-menu').fadeIn();
-		$(this).find('.arrow').fadeIn();
 		var isbgMenu = $(e.currentTarget).is('.bg-item');
 
 		if(isbgMenu){
+			$(this).find('.dropdown-menu').fadeIn();
+			$(this).find('.arrow').fadeIn();
 			$('.view-container').fadeIn();
 		}	
 
 		$('.view-container').click(function(e){
 			$(this).fadeOut();
 			$('.dropdown-menu').fadeOut();
-			$('.nav-item .arrow').fadeOut();
+			$('.bg-item .arrow').fadeOut();
 		});
+	});
 
-		//search-menu fadeOut
-		$('.dropdown-menu .icon-cancel-2').click(function(e){
-			$(this).closest('.dropdown-menu').fadeOut();
-		});
+	$('.li-1.searchs').click(function(e){
+		$(this).children('.dropdown-menu').toggleClass('active');
+	});
+	$('.icon-cancel-2').click(function(e){
+		$(this).parents('.dropdown-menu').removeClass('active');
 	});
 
 	//language choice
-	$('.language').click(function(){
-		$('.language-menu').toggleClass('active');
-	});
+	// $('.language').click(function(){
+	// 	$('.language-menu').toggleClass('active');
+	// });
 
 	//home-> featureeds prev-next
-	$('.prev-next a').click(function(e){
-		e.preventDefault();
-		$('a').removeClass('active');
-		$(this).addClass('active');
-	});
+	// $('.prev-next a').click(function(e){
+	// 	e.preventDefault();
+	// 	$('a').removeClass('active');
+	// 	$(this).addClass('active');
+	// });
 
 	//product-> detail
-	$('.tab li').click(function(e){
-		var index = $(e.currentTarget).index();
+	// $('.tab li').click(function(e){
+	// 	var index = $(e.currentTarget).index();
 		
-		$('.tab li').removeClass('active').eq(index).addClass('active');
-		$('.tab-content').removeClass('active').eq(index).addClass('active');
-	});
+	// 	$('.tab li').removeClass('active').eq(index).addClass('active');
+	// 	$('.tab-content').removeClass('active').eq(index).addClass('active');
+	// });
 
 	//popup
 	// $(document).scroll(function(){
